b33b98807646a87c6bc64baf27f52c3bab9e4b0f
VishalLa <149412023+VishalLa@users.noreply.github.com>
2026-07-06 10:50:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def stats(values: str):
 
     return stats_dict
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 8000))
     
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+#     uvicorn.run("main:app", host="0.0.0.0", port=port)
